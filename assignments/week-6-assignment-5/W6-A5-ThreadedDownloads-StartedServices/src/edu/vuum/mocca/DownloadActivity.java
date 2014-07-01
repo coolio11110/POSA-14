355310d4958dd1bbb9ac208d62e531e9c3dfb697
coolio11110 <tpwurzer@hotmail.com>
2014-07-01 16:40:28 -0700

    add 26-a5

--- assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-6-assignment-5/W6-A5-ThreadedDownloads-StartedServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -78,6 +78,7 @@ public class DownloadActivity extends DownloadBase {
                 // bitmap that's been downloaded and returned to
                 // the DownloadActivity as a pathname who's Bundle
             	// key is defined by DownloadUtils.PATHNAME_KEY
+            	activity.displayBitmap(msg.peekData().getString(DownloadUtils.PATHNAME_KEY));
             }
     	}
     }
@@ -107,6 +108,7 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // DownloadIntentService with the appropriate Intent
             // returned from the makeIntent() factory method.
+        	startService(DownloadIntentService.makeIntent(getApplicationContext(), handler, getUrlString()));
 
             which = "Starting IntentService";
             break;
@@ -115,6 +117,7 @@ public class DownloadActivity extends DownloadBase {
             // TODO - You fill in here to start the
             // ThreadPoolDownloadService with the appropriate Intent
             // returned from the makeIntent() factory method.
+        	startService(ThreadPoolDownloadService.makeIntent(getApplicationContext(), handler, getUrlString()));
 
             which = "Starting ThreadPoolDownloadService";
             break;
